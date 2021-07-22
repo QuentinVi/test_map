31f1f62abbe7ead1ae767c0364c47bb6975ecb7c
QuentinVi <57045060+QuentinVi@users.noreply.github.com>
2021-07-22 14:41:59 +0200

    Delete Interior 2.tsx

--- Interior 2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="Interior 2" tilewidth="32" tileheight="32" tilecount="832" columns="32">
- <image source="../../../Downloads/LPC Submissions/Interior 2.png" width="1024" height="832"/>
-</tileset>
